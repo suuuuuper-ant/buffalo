f0535e201c4cac09907d7ab58ae86d003bc1315f
jeong jin ho <zino@afreecatv.com>
2021-04-03 17:20:30 +0900

    seneDelegate 초기화면 나오도록 세팅

--- Digin/Digin/SceneDelegate.swift
+++ Digin/Digin/SceneDelegate.swift
@@ -12,10 +12,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        lauchandVisble()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
@@ -44,5 +41,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Called as the scene transitions from the foreground to the background.
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
+    }
+    
+    func lauchandVisble() {
+        guard let scene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene: scene)
+        self.window = window
+        self.window?.rootViewController = MainViewController()
+        self.window?.makeKeyAndVisible()
     }
 }
