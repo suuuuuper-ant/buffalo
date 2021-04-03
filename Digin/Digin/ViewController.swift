dd959bca6ac50b4625613617d0996cf18db22de1
jeong jin ho <zino@afreecatv.com>
2021-04-03 16:45:34 +0900

    프로젝트 생성 및 워크스페이스 추가

---
+++ Digin/Digin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Digin
+//
+//  Created by jinho jeong on 2021/04/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
