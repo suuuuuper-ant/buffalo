fd023e30e281f1a03e80e005cd85d49024ea79cb
jeong jin ho <zino@afreecatv.com>
2021-04-03 17:20:30 +0900

    seneDelegate 초기화면 나오도록 세팅

---
+++ Digin/Digin/MainViewController.swift
@@ -0,0 +1,27 @@
+//
+//  MainViewController.swift
+//  Digin
+//
+//  Created by jinho jeong on 2021/04/03.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        // Do any additional setup after loading the view.
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
