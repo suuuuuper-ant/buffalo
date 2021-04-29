2cc989dbabe0633ca5553d3973df48c0b77d9ff3
jeong jin ho <zino@afreecatv.com>
2021-04-29 11:51:16 +0900

    feat: slide open UI 컴포넌트만들기

--- Digin/Digin/Main/Home/View/HomeTitleHeaderView.swift
+++ Digin/Digin/Main/Home/View/HomeTitleHeaderView.swift
@@ -35,6 +35,15 @@ class HomeTitleHeaderView: UITableViewHeaderFooterView {
         randomPick.backgroundColor = .lightGray
         return randomPick
     }()
+
+    var slideOpenView: SlideOpenView = {
+        let slide = SlideOpenView(frame: CGRect(x: 0, y: 0, width: 40, height: 0))
+
+        slide.thumbnailViewStartingDistance = 6
+        slide.sliderCornerRadius = 80 / 2
+        return slide
+    }()
+
     override init(reuseIdentifier: String?) {
         super.init(reuseIdentifier: reuseIdentifier)
         setupUI()
@@ -55,11 +64,12 @@ class HomeTitleHeaderView: UITableViewHeaderFooterView {
         greetingLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 25).isActive = true
         greetingLabel.topAnchor.constraint(equalTo: dateLabel.bottomAnchor, constant: 50).isActive = true
 
-        addSubview(radomPickButton)
-        radomPickButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16).isActive = true
-        radomPickButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16).isActive = true
-        radomPickButton.heightAnchor.constraint(equalToConstant: 73).isActive = true
-        radomPickButton.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -20).isActive = true
+        addSubview(slideOpenView)
+        slideOpenView.translatesAutoresizingMaskIntoConstraints = false
+        slideOpenView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16).isActive = true
+        slideOpenView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16).isActive = true
+        slideOpenView.heightAnchor.constraint(equalToConstant: 80).isActive = true
+        slideOpenView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -20).isActive = true
 
     }
 
@@ -89,68 +99,149 @@ class HomeTitleHeaderView: UITableViewHeaderFooterView {
 
 }
 
+class SlideOpenView: UIView {
 
-//class SlideOpenView: UIView {
-//    
-//    
-//    let backgroundView = UIView()
-//    let indcatorView = UIView()
-//    let textLabel = UILabel()
-//    private var panGestureRecognizer: UIPanGestureRecognizer!
-//    
-//    public var thumbnailViewStartingDistance: CGFloat = 0.0 {
-//            didSet {
-////                leadingThumbnailViewConstraint?.constant = thumbnailViewStartingDistance
-////                trailingDraggedViewConstraint?.constant = thumbnailViewStartingDistance
-//                setNeedsLayout()
-//            }
+    let backgroundView = UIView()
+    let indicatorView = UIView()
+    let textLabel = UILabel()
+    public var sliderCornerRadius: CGFloat = 30.0 {
+            didSet {
+                backgroundView.layer.cornerRadius = sliderCornerRadius
+                indicatorView.layer.cornerRadius = sliderCornerRadius - thumbnailViewStartingDistance
+            }
+        }
+    private var panGestureRecognizer: UIPanGestureRecognizer!
+
+    public var thumbnailViewStartingDistance: CGFloat = 0.0 {
+        didSet {
+            indicatorHeightAnchor?.constant = -thumbnailViewStartingDistance * 2
+            indicatorLeadingAnchor?.constant =  thumbnailViewStartingDistance
+            setNeedsLayout()
+        }
+    }
+
+    private var xEndingPoint: CGFloat {
+            get {
+                return self.frame.width
+            }
+        }
+
+    private var initialWidth: CGFloat {
+        get {
+            return self.frame.width - self.frame.height
+        }
+    }
+
+    private var didSetup: Bool = false
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        setupView()
+        setupConstraints()
+    }
+
+    private func setupView() {
+
+        addSubview(backgroundView)
+
+        backgroundView.addSubview(textLabel)
+        backgroundView.addSubview(indicatorView)
+        backgroundView.translatesAutoresizingMaskIntoConstraints = false
+        indicatorView.translatesAutoresizingMaskIntoConstraints = false
+        textLabel.translatesAutoresizingMaskIntoConstraints = false
+        panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(self.handlePanGesture(_:)))
+        panGestureRecognizer.minimumNumberOfTouches = 1
+        indicatorView.addGestureRecognizer(panGestureRecognizer)
+
+        backgroundView.backgroundColor = .blue
+        indicatorView.backgroundColor = .lightGray
+
+        backgroundView.clipsToBounds = true
+        indicatorView.clipsToBounds = true
+        backgroundView.layer.cornerRadius = sliderCornerRadius
+        indicatorView.layer.cornerRadius = sliderCornerRadius - 3
+
+    }
+
+    var indicatorWidthAnchor: NSLayoutConstraint?
+    var indicatorHeightAnchor: NSLayoutConstraint?
+    var indicatorLeadingAnchor: NSLayoutConstraint?
+    private func setupConstraints() {
+
+        backgroundView.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+        backgroundView.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
+        backgroundView.topAnchor.constraint(equalTo: topAnchor).isActive = true
+        backgroundView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+
+        textLabel.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+        textLabel.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
+        textLabel.topAnchor.constraint(equalTo: topAnchor).isActive = true
+        textLabel.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+
+        indicatorLeadingAnchor = indicatorView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: thumbnailViewStartingDistance)
+        indicatorLeadingAnchor?.isActive = true
+        indicatorView.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        indicatorHeightAnchor = indicatorView.heightAnchor.constraint(equalTo: heightAnchor, constant: -thumbnailViewStartingDistance)
+        indicatorHeightAnchor?.isActive = true
+        indicatorWidthAnchor = indicatorView.widthAnchor.constraint(equalTo: indicatorView.heightAnchor)
+
+//        indicatorTrailingAnchor = indicatorView.trailingAnchor.constraint(equalTo: trailingAnchor)
+        indicatorWidthAnchor?.isActive = true
+
+    }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    @objc private func handlePanGesture(_ sender: UIPanGestureRecognizer) {
+
+        let translatedPoint = sender.translation(in: backgroundView).x
+
+        switch sender.state {
+        case .began:
+            break
+        case .changed:
+
+            if translatedPoint  >= initialWidth {
+                indicatorWidthAnchor?.constant = initialWidth
+            } else if translatedPoint <= 0 {
+                break
+            } else {
+
+                indicatorWidthAnchor?.constant = translatedPoint
+              //  thumbnailViewStartingDistance = translatedPoint
+            }
+
+        case .ended:
+
+            if translatedPoint >= (initialWidth / 2) {
+                indicatorWidthAnchor?.constant = initialWidth
+            } else {
+                let const = indicatorHeightAnchor?.constant ?? 0
+                let map = const + (thumbnailViewStartingDistance * 2)
+
+                indicatorWidthAnchor?.constant = map
+            }
+
+            UIView.animate(withDuration: 0.1, delay: 0.1, options: [.curveEaseIn]) {
+                self.layoutIfNeeded()
+            } completion: { _ in
+
+            }
+
+            break
+
+        default:
+            break
+        }
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+//        if !didSetup { indicatorTrailingAnchor?.constant = -(frame.width - frame.height)
+//            didSetup = true
 //        }
-//    
-//    private var xEndingPoint: CGFloat {
-//            get {
-//                return self.frame.maxX
-//            }
-//        }
-//    
-//    override init(frame: CGRect) {
-//        super.init(frame: frame)
-//    
-//    }
-//    
-//    
-//    private func setupView() {
-//        
-//        addSubview(backgroundView)
-//        
-//        backgroundView.addSubview(textLabel)
-//        backgroundView.addSubview(indcatorView)
-//        backgroundView.translatesAutoresizingMaskIntoConstraints = false
-//        indcatorView.translatesAutoresizingMaskIntoConstraints = false
-//        textLabel.translatesAutoresizingMaskIntoConstraints = false
-//        panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(self.handlePanGesture(_:)))
-//        panGestureRecognizer.minimumNumberOfTouches = 1
-//        backgroundView.addGestureRecognizer(panGestureRecognizer)
-//        
-//        
-//        backgroundView.leadingAnchor.constraint(equalTo: leadingAnchor)
-//        
-//    }
-//    required init?(coder: NSCoder) {
-//        fatalError("init(coder:) has not been implemented")
-//    }
-// 
-//    @objc private func handlePanGesture(_ sender: UIPanGestureRecognizer) {
-//        
-//        let translatedPoint = sender.translation(in: backgroundView).x
-//        
-//        switch sender.state {
-//        case .began:
-//            break
-//        case .changed
-//        
-//        
-//        default:
-//            break
-//        }
-//    }
-//}
+
+    }
+}
