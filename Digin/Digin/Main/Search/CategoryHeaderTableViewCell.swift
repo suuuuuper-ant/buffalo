c44b36c4d8e1677698c9bce11a8567c7903672ff
kyeahen <bright19961@naver.com>
2021-05-03 23:14:51 +0900

    feat: 검색 메인 화면 구성

---
+++ Digin/Digin/Main/Search/CategoryHeaderTableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  CategoryHeaderTableViewCell.swift
+//  Digin
+//
+//  Created by 김예은 on 2021/05/03.
+//
+
+import UIKit
+
+class CategoryHeaderTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+
+    @IBOutlet weak var lineView: UIView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+    }
+
+}
--- Digin/Digin/Main/Search/SearchViewController.swift
+++ Digin/Digin/Main/Search/SearchViewController.swift
@@ -9,20 +9,80 @@ import UIKit
 
 class SearchViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+
+    @IBOutlet weak var lineView: UIView!
+    @IBOutlet weak var searchTextField: UITextField!
+    @IBOutlet weak var textFieldLeadingC: NSLayoutConstraint!
+    @IBOutlet weak var searchButton: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setup()
     }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setup() {
+        tableView.delegate = self
+        tableView.dataSource = self
     }
-    */
+
+}
+
+// MARK: - TableView
+extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: CategoryHeaderTableViewCell.reuseIdentifier) as? CategoryHeaderTableViewCell else {
+            return UITableViewCell()
+        }
+
+        if section == 0 {
+            cell.titleLabel.text = "카테고리"
+            cell.timeLabel.isHidden = true
+        } else {
+            cell.titleLabel.text = "인기 검색 기업"
+            cell.timeLabel.isHidden = false
+            cell.timeLabel.text = "시간"
+        }
+
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+
+        if section == 0 {
+            return 1
+        } else {
+            return 5
+        }
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        if indexPath.section == 0 {
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: CategoryTableViewCell.reuseIdentifier) as? CategoryTableViewCell else {
+                return UITableViewCell()
+            }
+
+            cell.collectionView.reloadData()
+
+            return cell
+
+        } else {
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: CompanyTableViewCell.reuseIdentifier) as? CompanyTableViewCell else {
+                return UITableViewCell()
+            }
+
+            cell.titleLabel.text = "카카오"
+            cell.categoryLabel.text = "커뮤니케이션 서비스"
+
+            return cell
+        }
+    }
 
 }
