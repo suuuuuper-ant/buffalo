e92893779cdd1a0b28f66944714324fb3ca32b29
jeong jin ho <zino@afreecatv.com>
2021-05-07 22:31:48 +0900

    feat: Barchart view 생성

---
+++ Digin/Digin/Main/Home/View/HomeDetailBarChartCell.swift
@@ -0,0 +1,23 @@
+//
+//  HomeDetailBarChartCell.swift
+//  Digin
+//
+//  Created by jinho jeong on 2021/05/07.
+//
+
+import UIKit
+
+class HomeDetailBarChartCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
