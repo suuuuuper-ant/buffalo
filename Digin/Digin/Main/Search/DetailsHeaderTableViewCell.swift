956e3e144687bf8e64c97c4cda4cbfb5f5c2ccbc
kyeahen <bright19961@naver.com>
2021-05-10 04:28:51 +0900

    검색 메인 GUI 및 카테고리 작업

--- Digin/Digin/Main/Search/DetailsHeaderTableViewCell.swift
+++ Digin/Digin/Main/Search/DetailsHeaderTableViewCell.swift
@@ -1,23 +1,23 @@
 //
-//  CategoryHeaderTableViewCell.swift
+//  DetailsHeaderTableViewCell.swift
 //  Digin
 //
-//  Created by 김예은 on 2021/05/03.
+//  Created by 김예은 on 2021/05/10.
 //
 
 import UIKit
 
-class CategoryHeaderTableViewCell: UITableViewCell {
-
-    @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var timeLabel: UILabel!
-    @IBOutlet weak var nextButton: UIButton!
+class DetailsHeaderTableViewCell: UITableViewCell {
 
     @IBOutlet weak var lineView: UIView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var nextButton: UIButton!
 
     var nextClosure: (() -> Void)?
 
     @IBAction func nextAction(_ sender: UIButton) {
         self.nextClosure?()
     }
+
 }
