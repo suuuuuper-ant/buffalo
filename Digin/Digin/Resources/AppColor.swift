50a237c96ca6368616034c80e97aa1afe3d911de
jeong jin ho <zino@afreecatv.com>
2021-05-12 20:54:58 +0900

    resource: swiftgen추가

---
+++ Digin/Digin/Resources/AppColor.swift
@@ -0,0 +1,79 @@
+// swiftlint:disable all
+// Generated using SwiftGen — https://github.com/SwiftGen/SwiftGen
+
+#if os(macOS)
+  import AppKit
+#elseif os(iOS)
+  import UIKit
+#elseif os(tvOS) || os(watchOS)
+  import UIKit
+#endif
+
+// Deprecated typealiases
+@available(*, deprecated, renamed: "ColorAsset.Color", message: "This typealias will be removed in SwiftGen 7.0")
+internal typealias AssetColorTypeAlias = ColorAsset.Color
+
+// swiftlint:disable superfluous_disable_command file_length implicit_return
+
+// MARK: - Asset Catalogs
+
+// swiftlint:disable identifier_name line_length nesting type_body_length type_name
+internal enum AppColor {
+  internal static let darkgray62 = ColorAsset(name: "darkgray62")
+  internal static let darkgray52 = ColorAsset(name: "darkgray_52")
+  internal static let darkgray82 = ColorAsset(name: "darkgray_82")
+  internal static let dividerColor = ColorAsset(name: "divider_color")
+  internal static let gray160 = ColorAsset(name: "gray_160")
+  internal static let homeBackground = ColorAsset(name: "home_background")
+  internal static let mainColor = ColorAsset(name: "main_color")
+  internal static let pullIndicator = ColorAsset(name: "pull_indicator")
+  internal static let slidebarStartColor = ColorAsset(name: "slidebar_start_color")
+  internal static let stockBlue = ColorAsset(name: "stock_blue")
+  internal static let tagColor = ColorAsset(name: "tag_color")
+}
+// swiftlint:enable identifier_name line_length nesting type_body_length type_name
+
+// MARK: - Implementation Details
+
+internal final class ColorAsset {
+  internal fileprivate(set) var name: String
+
+  #if os(macOS)
+  internal typealias Color = NSColor
+  #elseif os(iOS) || os(tvOS) || os(watchOS)
+  internal typealias Color = UIColor
+  #endif
+
+  @available(iOS 11.0, tvOS 11.0, watchOS 4.0, macOS 10.13, *)
+  internal private(set) lazy var color: Color = Color(asset: self)
+
+  fileprivate init(name: String) {
+    self.name = name
+  }
+}
+
+internal extension ColorAsset.Color {
+  @available(iOS 11.0, tvOS 11.0, watchOS 4.0, macOS 10.13, *)
+  convenience init!(asset: ColorAsset) {
+    let bundle = BundleToken.bundle
+    #if os(iOS) || os(tvOS)
+    self.init(named: asset.name, in: bundle, compatibleWith: nil)
+    #elseif os(macOS)
+    self.init(named: NSColor.Name(asset.name), bundle: bundle)
+    #elseif os(watchOS)
+    self.init(named: asset.name)
+    #endif
+  }
+}
+
+// swiftlint:disable convenience_type
+private final class BundleToken {
+  static let bundle: Bundle = {
+    #if SWIFT_PACKAGE
+    return Bundle.module
+    #else
+    return Bundle(for: BundleToken.self)
+    #endif
+  }()
+}
+// swiftlint:enable convenience_type
