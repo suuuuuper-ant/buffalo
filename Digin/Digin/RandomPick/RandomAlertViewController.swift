a4cb0f05add915dd3c290d3fb682b2fe299d1ff1
jeong jin ho <zino@afreecatv.com>
2021-05-25 03:40:35 +0900

    bugfix: 얼럿라인덮어짐

--- Digin/Digin/RandomPick/RandomAlertViewController.swift
+++ Digin/Digin/RandomPick/RandomAlertViewController.swift
@@ -30,7 +30,6 @@ class RandomAlertViewController: UIViewController, ViewType {
         title.text = "그대로 나가시겠어요?"
         title.textColor = AppColor.darkgray62.color
         title.font = UIFont.systemFont(ofSize: 16, weight: .bold)
-        title.numberOfLines = 2
         return title
     }()
 
@@ -39,6 +38,7 @@ class RandomAlertViewController: UIViewController, ViewType {
         description.text = "랜덤 추천 기업은\n하루에 딱 한 번만 볼 수 있어요."
         description.textColor = AppColor.darkgray82.color
         description.font = UIFont.systemFont(ofSize: 14, weight: .medium)
+        title.numberOfLines = 2
         return description
     }()
 
