e4a39bc37b4d298f6d715536b722991caafdb7ad
kyeahen <bright19961@naver.com>
2021-05-27 21:36:38 +0900

    feat: #53 마이페이지 메인, 공지사항/약관 및 정책, 내정보 UI 작업

---
+++ Digin/Digin/Main/MyPage/MyPageMainTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  MyPageMainTableViewCell.swift
+//  Digin
+//
+//  Created by 김예은 on 2021/05/27.
+//
+
+import UIKit
+
+class MyPageMainTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+
+}
---
+++ Digin/Digin/Main/MyPage/NoticeTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  NoticeTableViewCell.swift
+//  Digin
+//
+//  Created by 김예은 on 2021/05/27.
+//
+
+import UIKit
+
+class NoticeTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+
+}
